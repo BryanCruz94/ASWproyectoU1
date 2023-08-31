9e7d59cfa9cc0aa145c4d153d304d04dac611ad8
DayanaV99 <dayanavergarabravo@gmail.com>
2023-08-31 15:27:27 -0500

    modificacionFotos

--- costa/tienda1.php
+++ costa/tienda1.php
@@ -29,10 +29,12 @@
             padding: 10px 0;
         }
 
-        
+
         .plato-img {
-            max-width: 100px;
+            width: 120px;
+            height: 100px;
             display: block;
+            padding: 5px ;
             margin: 0 auto;
             margin-top: 10px;
         }
@@ -49,7 +51,7 @@
     <div class="container">
         <form id="order-form" action="confirmacion.php" method="post">
             <div class="row">
-               
+
                 <div class="col-md-6 mb-3">
                     <label for="nombre" class="form-label">Nombre:</label>
                     <input type="text" name="nombre" class="form-control" required>
@@ -62,37 +64,60 @@
             <br>
             <h2>Platos:</h2>
             <div class="row">
-                <div class="col-md-6">
-                    <div class="form-check mb-2" class="col-md-6">
+                <div class="col-md-4">
+                    <div class="form-check mb-2">
                         <input type="checkbox" name="platos[]" value="Encocado" class="form-check-input">
                         <label class="form-check-label">Encocado - $5</label>
+                        <img src="https://www.bonella.com.ec/-/media/Project/Upfield/Brands/Rama/Rama-EC/Assets/Recipes/sync-img/1affceb4-de02-43ee-b315-afbd48b12f31.jpg?rev=ceefa8a393e0460c894cc7233897cd2f&w=900" alt="Encocado" class="plato-img">
+                        <br>
                         <input type="number" name="cantidades[0]" min="0" value="0" class="form-control">
-                        <img src="imgCosta/Encocado.jpg" alt="Encocado" class="plato-img">                    </div>
+                        <br>
+                    </div>
+                </div>
+                <div class="col-md-4">
                     <div class="form-check mb-2">
                         <input type="checkbox" name="platos[]" value="Encebollado" class="form-check-input">
                         <label class="form-check-label">Encebollado - $3</label>
+                        <img src="https://cloudfront-us-east-1.images.arcpublishing.com/eluniverso/ZAMW26AIKRCHJDWYXFEMQGX4KU.jpg" alt="Encebollado" class="plato-img">
+                        <br>
                         <input type="number" name="cantidades[1]" min="0" value="0" class="form-control">
-                    </div>
-                    <div class="form-check">
-                        <input type="checkbox" name="platos[]" value="Parrillada" class="form-check-input">
-                        <label class="form-check-label">Parrillada - $12</label>
-                        <input type="number" name="cantidades[2]" min="0" value="0" class="form-control">
+                        <br>
                     </div>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-4">
+                    <div class="form-check mb-2">
+                        <input type="checkbox" name="platos[]" value="Parrillada" class="form-check-input">
+                        <label class="form-check-label">Parrillada - $12</label>
+                        <img src="https://www.cocinavital.mx/wp-content/uploads/2023/03/parrillada-de-mariscos-todo-lo-que-necesitas.jpg" alt="Parrillada" class="plato-img">
+                        <br>
+                        <input type="number" name="cantidades[2]" min="0" value="0" class="form-control">
+                        <br>
+                    </div>
+                </div>
+                <div class="col-md-4">
                     <div class="form-check mb-2">
                         <input type="checkbox" name="platos[]" value="Tigrillo" class="form-check-input">
                         <label class="form-check-label">Tigrillo - $4</label>
+                        <img src="https://www.bonella.com.ec/-/media/Project/Upfield/Brands/Rama/Rama-EC/Assets/Recipes/sync-img/aee6aa7f-26b7-4327-b78b-051d06a86f51.jpg?rev=a5e10720d9dc445e89d96b438da6eb0a&w=900" alt="Tigrillo" class="plato-img">
+                        <br>
                         <input type="number" name="cantidades[3]" min="0" value="0" class="form-control">
                     </div>
+                </div>
+                <div class="col-md-4">
                     <div class="form-check mb-2">
                         <input type="checkbox" name="platos[]" value="Viche" class="form-check-input">
                         <label class="form-check-label">Viche - $3.5</label>
+                        <img src="https://i.ytimg.com/vi/LFpyzyH_W7c/maxresdefault.jpg" alt="Viche" class="plato-img">
+                        <br>
                         <input type="number" name="cantidades[4]" min="0" value="0" class="form-control">
                     </div>
-                    <div class="form-check">
+                </div>
+                <div class="col-md-4">
+                    <div class="form-check mb-2">
                         <input type="checkbox" name="platos[]" value="Cangrejada" class="form-check-input">
                         <label class="form-check-label">Cangrejada - $5.5</label>
+                        <img src="https://media-cdn.tripadvisor.com/media/photo-s/18/e7/73/51/la-cangrejada-ceviche.jpg" alt="Cangrejada" class="plato-img">
+                        <br>
                         <input type="number" name="cantidades[5]" min="0" value="0" class="form-control">
                     </div>
                 </div>
